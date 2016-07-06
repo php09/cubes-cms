d7e072e8991cee625f3f52fcc8ec8bbae3da1025
PHP09 <>
2016-07-06 15:18:10 +0200

    zadatak2

---
+++ application/controllers/Admin/ProfileController.php
@@ -0,0 +1,14 @@
+<?php
+
+class Admin_ProfileController extends Zend_Controller_Action
+{
+    
+    public function editAction() {
+        
+    }
+    
+    public function changepasswordAction() {
+        
+    }
+    
+}
